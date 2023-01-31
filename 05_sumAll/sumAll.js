f574ab17df2300338aead32311f36fd1d8fef918
joao-ar11 <jarlpereira29@gmail.com>
2023-01-31 16:56:58 -0300

    Solve 05_sumAll with initial solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(initial, end) {
+    if (typeof(initial) !== "number" || typeof(end) !== "number") {
+        return "ERROR";
+    }
+    if (initial < 0 || end < 0) {
+        return "ERROR";
+    }
+    let sum = 0;
+    let currentNumber = initial;
+    while (currentNumber !== end) {
+        sum += currentNumber;
+        if (initial < end) {
+            currentNumber++;
+        } else {
+            currentNumber--;
+        }
+    }
+    return sum + end;
 };
 
 // Do not edit below this line
