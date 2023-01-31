2424813f251eda5976d5e58c53dfc0dba9d6ec1a
joao-ar11 <jarlpereira29@gmail.com>
2023-01-31 15:07:46 -0300

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfRepeats) {
+    if (numberOfRepeats < 0) {
+        return "ERROR";
+    }
+    let resultString = "";
+    for (let i = 0; i < numberOfRepeats; i++) {
+        resultString += string;
+    }
+    return resultString;
 };
 
 // Do not edit below this line
