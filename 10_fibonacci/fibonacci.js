705b3eacb78fbe92605322019a37368a2afff039
joao-ar11 <jarlpereira29@gmail.com>
2023-02-11 14:39:24 -0300

    Solve 10_fibonacci with initial solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    number = +number;
+    if (number <= 0) return "OOPS";
+    let currentNumber = 1;
+    let pastNumber = 0;
+    let temp;
+    for (let i = 1; i < number; i++) {
+        temp = currentNumber;
+        currentNumber += pastNumber;
+        pastNumber = temp;
+    }
+    return currentNumber;
 };
 
 // Do not edit below this line
